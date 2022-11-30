1802d56ec9b7f22efbeb93ddc056be03cd1c865e
Silvio Fricke <silvio.fricke@softing.com>
2022-11-30 15:59:05 +0100

    HACK: remove reference to btrfs, we doesn't use btrfs on android
    TODO: find coresponding commit on origin/master. On o/m the entry is
    missing. Someone had removed it already.
    
    Signed-off-by: Silvio Fricke <silvio.fricke@softing.com>

--- cmd/containerd/builtins_linux.go
+++ cmd/containerd/builtins_linux.go
@@ -22,7 +22,6 @@ import (
 	_ "github.com/containerd/containerd/runtime/v1/linux"
 	_ "github.com/containerd/containerd/runtime/v2"
 	_ "github.com/containerd/containerd/runtime/v2/runc/options"
-	_ "github.com/containerd/containerd/snapshots/btrfs/plugin"
 	_ "github.com/containerd/containerd/snapshots/native/plugin"
 	_ "github.com/containerd/containerd/snapshots/overlay/plugin"
 	_ "github.com/containerd/fuse-overlayfs-snapshotter/plugin"
